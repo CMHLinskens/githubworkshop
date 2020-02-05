fa0265a191af556214fb72d5d3431807f6263184
Conner Linskens <cmh.linskens@student.avans.nl>
2020-02-05 11:32:52 +0100

    Solved merge conflict and added extra test

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,12 @@ public class Main {
 
     public static void main(String[] args){
         System.out.println("Hoi.");
-        System.out.println("Hoi nog een keer.");
-        System.out.println("Hoi nog een keer.");
 
         double a = 3.3;
         double b = 6.6;
+        System.out.println(sum(a,b));
         System.out.println(sub(a,b));
-        System.out.println(sum(a,b) + mul(a,b));
+        System.out.println(mul(a,b));
     }
 
     public static double sum(double a, double b){
@@ -16,10 +15,10 @@ public class Main {
     }
 
     public static double sub(double a, double b){
-        return a+b;
+        return a-b;
     }
 
     public static double mul(double a, double b){
-        return a+b;
+        return a*b;
     }
 }
