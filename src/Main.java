788929527ba5e883bfaad5b9d0072e59d1f72a03
Conner Linskens <cmh.linskens@student.avans.nl>
2020-02-05 11:23:59 +0100

    Added method subtract

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,14 @@ public class Main {
         double a = 3.3;
         double b = 6.6;
         System.out.println(sum(a,b));
+        System.out.println(sub(a,b));
     }
 
     public static double sum(double a, double b){
+        return a+b;
+    }
+
+    public static double sub(double a, double b){
         return a+b;
     }
 }
