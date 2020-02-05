4b3a20a071449196a9a2ed5fa986cc72165173ed
Conner Linskens <cmh.linskens@student.avans.nl>
2020-02-05 11:20:14 +0100

    Added method multiply

--- src/Main.java
+++ src/Main.java
@@ -7,10 +7,14 @@ public class Main {
 
         double a = 3.3;
         double b = 6.6;
-        System.out.println(sum(a,b));
+        System.out.println(sum(a,b) + mul(a,b));
     }
 
     public static double sum(double a, double b){
+        return a+b;
+    }
+
+    public static double mul(double a, double b){
         return a+b;
     }
 }
